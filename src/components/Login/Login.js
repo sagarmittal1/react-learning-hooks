fa477696d26401147f473bea73a3d40af3341693
Sagar Mittal <mittalsagar006@gmail.com>
2022-11-11 15:59:16 +0530

    learned useEffect hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,30 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // for learning purpose
+  useEffect(() => {
+    console.log('Effect running');
+
+    return () => {
+      console.log('EFFECT CLEANUP');
+    }
+  }, [enteredPassword]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      // console.log('checking validity');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
